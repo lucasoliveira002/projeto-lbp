689555bc5f82e6b3891306206468679764badbaa
Lucas Oliveira <135052326+lucasoliveira002@users.noreply.github.com>
2024-12-13 18:24:11 -0300

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, session
 from controllers.professores_controller import professores_bp
 from controllers.login_controller import login_bp
 from controllers.cursos_controller import cursos_bp
+from controllers.ia_controller import ia_bp
 from database import init_db
 import os
 
@@ -12,9 +13,14 @@ app.secret_key = os.urandom(24)
 def pagina_inicial():
     return render_template("home.html")
 
+@app.route("/ia")
+def ia():
+    return render_template('ia.html')
+
 app.register_blueprint(professores_bp, url_prefix='/professores')
 app.register_blueprint(login_bp, url_prefix='/login')
 app.register_blueprint(cursos_bp, url_prefix='/cursos')
+app.register_blueprint(ia_bp, url_prefix='/ia')
 
 
 if __name__ == "__main__":
